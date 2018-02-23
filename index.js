388f886784821a38e7470c0daf89447b60d9588e
Bluecat IBM <blhelpde@in.ibm.com>
2018-02-23 16:25:51 +0530

    .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 3000;
+var port = 3005;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
